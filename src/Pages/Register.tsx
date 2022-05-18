f5a4cd3ce713ff041e81d173cfc0f5480add0767
Arnoldo Solis <arnoldosolis817@gmail.com>
2022-05-18 01:08:10 -0400

    Added Yup for registration

--- src/Pages/Register.tsx
+++ src/Pages/Register.tsx
@@ -1,55 +1,110 @@
-import React from "react";
-import { Formik, Form } from "formik";
+import React, { useState } from "react";
+import { Formik, Form, Field } from "formik";
 import FormWrapper from "../Components/FormWrapper";
-import { gql, useMutation } from "@apollo/client";
+import { gql, useMutation, useQuery } from "@apollo/client";
+import * as Yup from "yup";
+import { Button } from "@mui/material";
 
 const REGISTER = gql`
   mutation Register($email: String!, $password: String!) {
-    register(options: { email: $email, password: $password }) {
-      errors {
-        field
-        message
-      }
-      user {
-        id
-        email
-      }
+    register(user: { email: $email, password: $password }) {
+      id
+    }
+  }
+`;
+
+const GET_USER_BY_EMAIL = gql`
+  query getUserByEmail($email: String!) {
+    getUserByEmail(user: { email: $email }) {
+      id
     }
   }
 `;
 
 function Register() {
   const [register] = useMutation(REGISTER);
+  const [email, setEmail] = useState("EMPTY");
+  const { data } = useQuery(GET_USER_BY_EMAIL, {
+    variables: { email: email },
+  });
+  const registerSchema = Yup.object().shape({
+    email: Yup.string()
+      .email()
+      .required("Email is required")
+      .test("Unique Email", "Email already in use", () => {
+        return !!data;
+      }),
+    password: Yup.string()
+      .required("Please specify a password")
+      .min(2, "Too Short!")
+      .max(50, "Too Long!"),
+  });
   return (
     <FormWrapper variant={{ variant: "small" }}>
       <Formik
         initialValues={{ email: "", password: "" }}
-        onSubmit={(values) => {
-          console.log(values);
-          return register(values);
+        validationSchema={registerSchema}
+        validateOnChange={false}
+        onSubmit={async (values) => {
+          // console.log(values);
+          setEmail(values.email);
+          register({
+            variables: {
+              email: values.email,
+              password: values.password,
+            },
+          });
         }}
       >
-        {({ values, handleChange }) => (
+        {({ values, errors }) => (
           <Form>
             <div>
               <label id="email">Email </label>
-              <input
-                value={values.email}
-                onChange={handleChange}
+              <Field
                 id="email"
                 type="email"
+                name="email"
+                value={values.email}
               />
             </div>
+            {errors.email && (
+              <div
+                style={{
+                  color: "red",
+                  fontSize: "0.8rem",
+                  marginLeft: "1.5rem",
+                }}
+              >
+                {errors.email}
+              </div>
+            )}
             <div style={{ marginTop: "4px" }}>
               <label id="password">Password </label>
-              <input
-                value={values.password}
-                onChange={handleChange}
+              <Field
                 id="password"
                 type="password"
+                name="password"
+                value={values.password}
               />
             </div>
-            <button type="submit">Register</button>
+            {errors.password && (
+              <div
+                style={{
+                  color: "red",
+                  fontSize: "0.8rem",
+                  marginLeft: "1.5rem",
+                }}
+              >
+                {errors.password}
+              </div>
+            )}
+            <Button
+              style={{ marginTop: "4px" }}
+              variant="contained"
+              type="submit"
+            >
+              Register
+            </Button>
           </Form>
         )}
       </Formik>
